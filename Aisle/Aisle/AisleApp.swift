714dde18cbce68247631345f24b76a10c6ab2645
Rushikesh Suradkar <rushikeshsuradkar2000@gmail.com>
2025-08-04 00:54:49 +0530

    project setup commit

---
+++ Aisle/Aisle/AisleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AisleApp.swift
+//  Aisle
+//
+//  Created by Rushikesh Suradkar on 04/08/25.
+//
+
+import SwiftUI
+
+@main
+struct AisleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
